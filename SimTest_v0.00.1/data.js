47e9abb81ec01750cc6b6804a393e9b796297622
TeaCloud <93931389+Antivirus95@users.noreply.github.com>
2025-11-16 13:26:45 +0200

    Add files via upload

---
+++ SimTest_v0.00.1/data.js
@@ -0,0 +1,62 @@
+export const questions = [
+    {
+        qw: "Як називається зелений кристал над головою Сіма?",
+        ans: ["Plumbob", "Simorb", "Greendiamond", "Moodgem"],
+        correct: "Plumbob",
+        img: "testte.jpg"
+    },
+    {
+        qw: "Хто створив серію ігор The Sims?",
+        ans: ["Sid Meier", "Will Wright", "Todd Howard", "Gabe Newell"],
+        correct: "Will Wright",
+        img: "testte.jpg"
+    },
+    {
+        qw: "Яке вигадане сімейство є найвідомішим у серії The Sims?",
+        ans: ["Lothario", "Caliente", "Goth", "Landgraab"],
+        correct: "Goth",
+        img: "testte.jpg"
+    },
+    {
+        qw: "Якою мовою розмовляють персонажі у The Sims?",
+        ans: ["Simlish", "Gibberish", "Plumbish", "Simerian"],
+        correct: "Simlish",
+        img: "testte.jpg"
+    },
+    {
+        qw: "Що станеться, якщо зняти драбину з басейну в The Sims 1?",
+        ans: ["Сім утоне", "Сім вилізе", "Сім засне", "Нічого не станеться"],
+        correct: "Сім утоне",
+        img: "testte.jpg"
+    },
+    {
+        qw: "Яка потреба символізується іконкою з туалетом?",
+        ans: ["Fun", "Energy", "Bladder", "Hunger"],
+        correct: "Bladder",
+        img: "testte.jpg"
+    },
+    {
+        qw: "Як називається режим, у якому будують будинки?",
+        ans: ["Live Mode", "Create-A-Sim", "Build Mode", "Edit Mode"],
+        correct: "Build Mode",
+        img: "testte.jpg"
+    },
+    {
+        qw: "Хто є сусідом сім'ї Готів у The Sims 4 (у Верде Крик)?",
+        ans: ["Landgraab", "Caliente", "Lothario", "Pancakes"],
+        correct: "Pancakes",
+        img: "testte.jpg"
+    },
+    {
+        qw: "Як звати дружину Боба Панкейка у The Sims 4?",
+        ans: ["Bella", "Eliza", "Nina", "Nancy"],
+        correct: "Eliza",
+        img: "testte.jpg"
+    },
+    {
+        qw: "Яка знаменита фраза стала мемом серед фанатів The Sims?",
+        ans: ["Sul sul!", "Yare Yare Daze", "Muda Muda!", "Woohoo!"],
+        correct: "Sul sul!",
+        img: "testte.jpg"
+    }
+];
